42d3f2dc9a85376c2e6b90a3dbe26d368fae3423
Joshua Castillo <jostillo91@gmail.com>
2025-11-10 20:34:03 -0700

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/3D-portfolio',
+  assetPrefix: '/3D-portfolio',
 }
 
 module.exports = nextConfig
